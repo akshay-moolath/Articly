837d03420e6cbafeb9ac3814250c1d1b8ba600f4
akshay-moolath <akmoolath@gmail.com>
2025-11-27 13:47:43 +0530

    made simple static pages for login,register and for dashboard and created main.py to make fastapi

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles#for getting simple html page for login,register and dashboard
+from fastapi.responses import FileResponse
+
+
+app = FastAPI()
+
+
+@app.get("/")# connecting static pages to url/endpoint
+def home():
+    return FileResponse("static/login.html")
+
+
+@app.get("/register-page")
+def register_page():
+    return FileResponse("static/register.html")
+
+
+@app.get("/dashboard")
+def dashboard_page():
+    return FileResponse("static/dashboard.html")
