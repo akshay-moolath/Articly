f98e0365681b7d31caa3961c8a38bdc7dd760859
akshay-moolath <akmoolath@gmail.com>
2025-11-28 16:19:42 +0530

    created new venv to change the name of project

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\akmoo\Downloads\Python Projects\3.Articly\.venv"
+set "VIRTUAL_ENV=C:\Users\akmoo\Downloads\Python Projects\3.Booky\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\akmoo\Downloads\Python Projects\3.Articly\.venv'
+set -gx VIRTUAL_ENV 'C:\Users\akmoo\Downloads\Python Projects\3.Booky\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
